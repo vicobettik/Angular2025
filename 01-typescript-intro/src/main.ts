e32c908240d37b783bb72bc609b29f7bb6ec4a1a
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2025-12-29 09:12:38 -0600

    refuerzo de interfaces

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -3,8 +3,8 @@ import './style.css'
 // requerir un modulo
 // import './topics/01-basic-types'
 // import './topics/02-object-inteface'
-import './topics/03-functions'
-
+// import './topics/03-functions'
+import './topics/04-homework.types'
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = 'Hola mundo';
--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -5,10 +5,39 @@ function addNumbers(a: number,b: number){
 
 const addNumbersArrow = (a:number, b:number):string => {
     return `${a + b}`;
-} 
+}
 
-const result = addNumbers(1,2);
-const result2 = addNumbersArrow(1,2);
+function multiply(fristNumber: number, secondNumber?: number, base: number = 2){
+    return fristNumber * base;
+}
 
-console.log({result});
-console.log({result2});
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+const vico: Character = {
+    name: 'vico',
+    hp: 50,
+    showHp(){
+        console.log(`Puntos de vida ${this.hp}`)
+    }
+}
+
+const healCharacter = (character: Character, amount:number) => {
+
+    character.hp += amount;
+}
+
+healCharacter(vico, 10);
+
+vico.showHp();
+
+// const result = addNumbers(1,2);
+// const result2 = addNumbersArrow(1,2);
+// const multiplyResult = multiply(5)
+
+// console.log({result});
+// console.log({result2});
+// console.log({multiplyResult})
