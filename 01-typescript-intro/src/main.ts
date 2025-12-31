d7785384f83d88ee63b8783baea8edf98a4a79bd
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2025-12-30 19:40:32 -0600

    se agrega archivo para decoradores

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -9,7 +9,8 @@ import './style.css'
 // import './topics/06-function-destructuring'
 // import './topics/07-import-export'
 // import './topics/08-classes'
-import './topics/09-generics'
+// import './topics/09-generics'
+import './topics/10-decorators'
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
