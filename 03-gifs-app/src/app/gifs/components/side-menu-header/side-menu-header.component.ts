f4293ff346d5e536ef84c29503d2a0e3b0603a43
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2026-01-09 14:06:13 -0600

    se agrega html para mostrar gifs

--- 03-gifs-app/src/app/gifs/components/side-menu-header/side-menu-header.component.ts
+++ 03-gifs-app/src/app/gifs/components/side-menu-header/side-menu-header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
+
 
 @Component({
   selector: 'gifs-side-menu-header',
@@ -7,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './side-menu-header.component.css'
 })
 export class SideMenuHeaderComponent {
+
+  envs = environment;
 
 }
