74325efd8c2bfee308a6b2003c0fa150065b325a
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2026-01-19 11:43:39 -0600

    se termina app de gifs

--- 03-gifs-app/src/app/gifs/pages/gif-history/gif-history.component.ts
+++ 03-gifs-app/src/app/gifs/pages/gif-history/gif-history.component.ts
@@ -1,18 +1,28 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { GifsService } from '../../services/gifs.service';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
 
 @Component({
   selector: 'app-gif-history',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './gif-history.component.html',
   styleUrl: './gif-history.component.css'
 })
 export class GifHistoryComponent {
 
-  query = inject(ActivatedRoute).params.subscribe(
-    () => {
+  gifService = inject(GifsService);
 
-    }
-  )
+  query = toSignal(
+    inject(ActivatedRoute).params.pipe(
+      map((params) => params['query'])
+    )
+  );
+
+  gifsByKey = computed(() => {
+    return this.gifService.getHistoryGifs(this.query());
+  })
 
 }
--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,17 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { computed, inject, Injectable, signal } from '@angular/core';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
-import { map, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
+
+const GIF_KEY = 'searchHistory';
+
+const loadFromLocalStorage = () => {
+  const gifsHistory = localStorage.getItem(GIF_KEY);
+  return gifsHistory ? JSON.parse(gifsHistory) : [];
+}
 
 @Injectable({
   providedIn: 'root'
@@ -16,13 +23,17 @@ export class GifsService {
   trendingGifs = signal<Gif[]>([]);
   trendingGifsLoading = signal(true);
 
-  searchHistory = signal<Record<string, Gif[]>>({});
+  searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
   searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
 
   constructor() {
     this.loadTrendingGifs();
 
   }
+
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem(GIF_KEY,JSON.stringify(this.searchHistory()));
+  })
 
   loadTrendingGifs(){
 
@@ -40,7 +51,7 @@ export class GifsService {
 
   };
 
-  searchGifs(query: string){
+  searchGifs(query: string): Observable<Gif[]>{
 
     return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`,
       {
@@ -51,9 +62,6 @@ export class GifsService {
         }
       }
     ).pipe(
-      // tap((resp) => {
-      //   console.log({tap:resp});
-      // }),
       map(({data}) => data),
       map((items) => GifMapper.mapGiphyItemsToGifArray(items)),
       tap((items) => {
@@ -63,13 +71,11 @@ export class GifsService {
         }))
       })
 
-      //TODO historial
     );
-    // .subscribe((resp) => {
-    //   const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
-    //   console.log({gifs})
-    // })
+  }
 
+  getHistoryGifs(query: string): Gif[]{
+    return this.searchHistory()[query] ?? []
   }
 
 }
