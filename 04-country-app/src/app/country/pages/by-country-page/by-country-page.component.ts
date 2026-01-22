12494ea7789eaf0c395148457cd69e8e913e7e3c
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2026-01-22 13:22:42 -0600

    creacion de componentes, paginas y rutas para la country app

---
+++ 04-country-app/src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { SearchInputComponent } from "../../components/search-input/search-input.component";
+import { CountryListComponent } from "../../components/country-list/country-list.component";
+
+@Component({
+  selector: 'app-by-country-page',
+  imports: [SearchInputComponent, CountryListComponent],
+  templateUrl: './by-country-page.component.html',
+  styleUrl: './by-country-page.component.css'
+})
+export class ByCountryPageComponent {
+onSearch($event: string) {
+throw new Error('Method not implemented.');
+}
+
+}
---
+++ 04-country-app/src/app/country/pages/country-page/country-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-country-page',
+  imports: [],
+  templateUrl: './country-page.component.html',
+  styleUrl: './country-page.component.css'
+})
+export class CountryPageComponent {
+
+
+
+}
