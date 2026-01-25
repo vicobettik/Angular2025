788f70b91f5c389e71febb419b3412719a20a328
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2026-01-24 19:24:59 -0600

    inicio resources

--- 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { CountryService } from '../../services/country.service';
@@ -13,28 +13,44 @@ import { Country } from '../../interfaces/country.interface';
 })
 export class ByCapitalPageComponent {
   countryService = inject(CountryService);
+  query = signal('');
 
-  isLoading = signal(false);
-  isError = signal<string | null>(null);
-  countries = signal<Country[]>([]);
-
-  onSearch(value: string) {
-    if (this.isLoading()) {
-      return;
-    }
-    this.isLoading.set(true);
-    this.isError.set(null);
-
-    this.countryService.searchByCaptal(value).subscribe({
-      next: (countries) => {
-        this.isLoading.set(false);
-        this.countries.set(countries);
-      },
-      error: (err) => {
-        this.isLoading.set(false);
-        this.countries.set([]);
-        this.isError.set(err)
-      }
-    });
+  onSearch(value:string){
+    this.query.set(value);
   }
+
+  countryResource = resource({
+
+    request: () => ({query: this.query()}),
+    loader: async({}) => {
+      if (!this.query()) {
+        return [];
+      }
+
+    }
+  })
+
+  // isLoading = signal(false);
+  // isError = signal<string | null>(null);
+  // countries = signal<Country[]>([]);
+
+  // onSearch(value: string) {
+  //   if (this.isLoading()) {
+  //     return;
+  //   }
+  //   this.isLoading.set(true);
+  //   this.isError.set(null);
+
+  //   this.countryService.searchByCaptal(value).subscribe({
+  //     next: (countries) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set(countries);
+  //     },
+  //     error: (err) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set([]);
+  //       this.isError.set(err)
+  //     }
+  //   });
+  // }
 }
