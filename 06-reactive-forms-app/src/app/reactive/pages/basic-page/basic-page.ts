642c0a790de7ed38307843750ef4e90c1be85ede
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2026-02-06 19:22:43 -0600

    formularios con formbuilder

--- 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -10,10 +10,19 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class BasicPage {
 
-  myForm = new FormGroup({
-    name: new FormControl(''),
-    price: new FormControl(0),
-    inStorage: new FormControl(0),
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  private fb = inject(FormBuilder);
+
+  // name: ['',/** validadores sincronos */,/** validadores asincronos */],
+  myForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0,[Validators.required, Validators.min(10)]],
+    inStorage: [0,[Validators.required, Validators.min(10)]],
   })
 
 }
