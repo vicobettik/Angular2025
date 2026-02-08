4954d2ec36de71f7a2cf9ac0ee582eb4a88a9940
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2026-02-08 10:27:31 -0600

    inicio formulario dinamico

--- 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.ts
@@ -1,12 +1,31 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/formUtils';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.html',
   styleUrl: './dynamic-page.css',
 })
 export class DynamicPage {
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array(
+      [
+        ['metal gear', [Validators.required]],
+        ['ori', [Validators.required]],
+      ],
+      [Validators.minLength(3)],
+    ),
+  });
+
+  get favoriteGames(){
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
 
 }
