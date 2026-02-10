937b31b0bb9cf9577ac550f8f19747c6ae9baf68
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2026-02-10 14:17:03 -0600

    formulario con radio, check

--- 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.ts
@@ -1,12 +1,25 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/formUtils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.html',
   styleUrl: './switches-page.css',
 })
 export class SwitchesPage {
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
 
+  myForm: FormGroup = this.fb.group({
+    gender: [null, [Validators.required]],
+    wantNotification: [true],
+    termsAndConditions: [false, [Validators.requiredTrue]],
+  });
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
