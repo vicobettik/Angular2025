1c45f193d99a2383deb354cce32804dfb2ba7a79
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2026-02-14 13:28:35 -0600

    fin de formularios reactivos, casacada

--- 06-reactive-forms-app/src/app/country/pages/country-page/country-page.ts
+++ 06-reactive-forms-app/src/app/country/pages/country-page/country-page.ts
@@ -3,7 +3,7 @@ import { Component, effect, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../interfaces/country.interface';
-import { switchMap, tap } from 'rxjs';
+import { catchError, filter, of, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -12,7 +12,6 @@ import { switchMap, tap } from 'rxjs';
   styleUrl: './country-page.css',
 })
 export class CountryPage {
-
   fb = inject(FormBuilder);
   countryService = inject(CountryService);
 
@@ -22,37 +21,68 @@ export class CountryPage {
   borders = signal<Country[]>([]);
 
   myForm = this.fb.group({
-    region:['',[Validators.required]],
-    country:['',[Validators.required]],
-    border:['',[Validators.required]],
+    region: ['', [Validators.required]],
+    country: ['', [Validators.required]],
+    border: ['', [Validators.required]],
   });
 
   onFormChanged = effect((onCleanUp) => {
-
     const regionSuscription = this.onRegionChanged();
+    const countrySusctiption = this.onCountryChanged();
 
     onCleanUp(() => {
       regionSuscription?.unsubscribe();
-    })
-
+      countrySusctiption.unsubscribe();
+    });
   });
 
-  onRegionChanged(){
-    return this.myForm.get('region')!.valueChanges
-      .pipe(
+  onRegionChanged() {
+    return this.myForm
+      .get('region')!
+      .valueChanges.pipe(
         tap(() => this.myForm.get('country')?.setValue('')),
         tap(() => this.myForm.get('border')?.setValue('')),
         tap(() => {
           this.borders.set([]);
           this.countriesByRegion.set([]);
         }),
-        switchMap((region) => this.countryService.getCountriesByRegion(region ?? ''))
+        switchMap((region) => {
+          return this.countryService.getCountriesByRegion(region!)
+          .pipe(
+            catchError((error) => {
+              console.log(error)
+              return of([])
+            })
+          )
+        })
       )
       .subscribe((countries) => {
+        console.log(countries)
         this.countriesByRegion.set(countries);
+      });
+  };
+
+  onCountryChanged(){
+
+    return this.myForm.get('country')!.valueChanges
+    .pipe(
+      tap(() => this.myForm.get('border')!.setValue('')),
+      filter((value) => value!.length > 0),
+      switchMap((countryCode) => this.countryService.getCountryByAlphaCode(countryCode!)
+        .pipe(
+          catchError((error) => of(null))
+        )
+      ),
+      switchMap((country) => {
+        if (!country || !country.borders || country.borders.length === 0) {
+          return ([]);
+        }
+        return this.countryService.getCountryNamesByCountryCodes(country?.borders)
       })
+    )
+    .subscribe((countries) => {
+      this.borders.set(countries);
+    })
+
   }
-
-
-
 }
--- 06-reactive-forms-app/src/app/country/services/country.service.ts
+++ 06-reactive-forms-app/src/app/country/services/country.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { combineLatest, forkJoin, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
 @Injectable({ providedIn: 'root' })
@@ -33,7 +33,19 @@ export class CountryService {
 
   };
 
-  getCountryBorderByCode(borders:string[]){
-    //todo
+  getCountryNamesByCountryCodes(countryCodes:string[]):Observable<Country[]>{
+    if (!countryCodes || countryCodes.length === 0) {
+      return of([])
+    }
+
+    const countriesRequests:Observable<Country>[] = [];
+
+    countryCodes.forEach((code) => {
+      const request = this.getCountryByAlphaCode(code);
+      countriesRequests.push(request);
+    });
+
+    return forkJoin(countriesRequests);
+
   }
 }
